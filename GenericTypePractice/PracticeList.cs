0c6e54f108ac588b2bdcb04334c1967470915d99
Eric Thomas <eric.therock@gmail.com>
2018-08-04 11:51:03 -0500

    Practice with generics

---
+++ GenericTypePractice/PracticeList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypePractice
+{
+    class PracticeList<T>
+    {
+        private T[] items;
+
+        public PracticeList()
+        {
+            items = new T[0];
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void Add(T newItem)
+        {
+            T[] newItems = new T[items.Length + 1];
+
+            for (int index = 0; index < items.Length; index++)
+            {
+                newItems[index] = items[index];
+
+                newItems[newItems.Length - 1] = newItem;
+
+                items = newItems;
+            }
+
+        }
+    }
+}
